4c273a9e594b350db8b7fef7f64264f415e5b857
adisak <adisak.suk@rmutsvmail.com>
2020-07-15 14:25:46 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+import com.sun.deploy.uitoolkit.impl.fx.AppletStageManager;
+public class HelloWorld
+{
+    public static void main(String[] args) {
+        System.out.println("HolloWorld");
+
+
+
+
+
+
+
+    }
+
+    }
+
+
