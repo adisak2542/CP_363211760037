0f82ed9c788ffe8a4792eacfd527471dca98df60
adisak <adisak.suk@rmutsvmail.com>
2020-07-15 14:56:14 +0700

    add more code

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("HolloWorld");
+        System.out.println("อดิศักดิ์ สุขศรีเมือง");
+        System.out.println("363211760037");
+        System.out.println("เทคโนโลยีการจัดการ");
+        System.out.println("การจัดการเทคโนโลยีสารสนเทศ");
+
 
 
 
