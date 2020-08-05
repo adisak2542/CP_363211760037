ec4193fbcf6be1ba521d38c8ac1138440412802f
STP_COM08 <adisak.suk@rmutsvmail.com>
2020-08-05 15:49:04 +0700

    adisak1-2-3

---
+++ src/Exercise/Chapter4_2.java
@@ -0,0 +1,22 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Chapter4_2 {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter integer: ");
+        int a = scanner.nextInt();//int
+        System.out.println("Enter rael number:");
+        double b = scanner.nextInt();//double
+        System.out.println("Enter integer: ");
+        int c = scanner.nextInt();// int
+        System.out.println("Enter rael number:");
+        double d = scanner.nextDouble();//double
+        //display
+        System.out.println(a+ " "+b+" "+c+" "+d);
+
+    }
+}
